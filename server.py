63b67c0e318de48f1cf57a21caf0ba414f4ba952
mackong <mackonghp@gmail.com>
2025-05-17 11:38:25 +0800

    merge main

--- server.py
+++ server.py
@@ -118,7 +118,6 @@ def load_model(model_name, huggingface_path=None, system="", temperature=1.0, Fr
     context_length = get_context_length(model_name, config.get_path("models"))
 
     
-    modele_rkllm = RKLLM(os.path.join(model_dir, from_value))
     modele_rkllm = RKLLM(os.path.join(model_dir, from_value), model_dir, temperature=float(temperature), context_length=context_length)
 
     if image_emb_model_path is not None:
